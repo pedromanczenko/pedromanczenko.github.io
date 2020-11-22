ff866331a394b6e914fed8221b2797cea61987eb
pedromanczenko <43644467+pedromanczenko@users.noreply.github.com>
2020-11-22 15:21:31 -0300

    Add files via upload

--- SP_test/site/main.js
+++ SP_test/site/main.js
@@ -1,38 +1,72 @@
-/*
-const parallax = document.querySelector('.parallax');
-
-window.addEventListener('scroll', () => {
-    let offset = window.pageYOffset;
-    parallax.style.backgroundPositionY = offset * .4 + 'px';
-});
-*/
-
-/*
-const header = document.querySelector('.sectionObserved');
-const navbar = document.querySelector('.navbar');
-const options = {
-    root: null,
-    threshold: 0,
-    rootMargin: '-60px'
-};
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            navbar.classList.remove('variation');
-        } else {
-            navbar.classList.add('variation');
-        } 
-    });
-},options);
-observer.observe(header);
-*/
-
-
 const navbar = document.querySelector('.navbar');
 window.addEventListener('scroll', () => {
     navbar.classList.toggle('variation', window.scrollY > 0);
 });
 
+const cards = document.querySelectorAll('.card');
+const arrows = document.querySelectorAll('.arrow');
+const dots = document.querySelectorAll('.dot');
+arrows.forEach(arrow => {
+    arrow.addEventListener('click', () => {
+        if (arrow.classList.contains('left')) {
+            backward() 
+        } else {
+            forward()
+        }
+    });
+});
+let current = 0;
+
+dots.forEach(dot => {
+    dot.addEventListener('click', () => {
+        current = [...dots].indexOf(dot); 
+        removeMark();
+        goto(current);
+        dot.classList.add('marked');
+    });
+});
+
+function removeMark() {
+    dots.forEach(dot => {
+        dot.classList.remove('marked');
+    });
+}
+
+function forward() {
+    if (current === 3) {
+        current = 0;
+    } else {
+        current++
+    } 
+    goto(current);
+}
+
+function backward() {
+    if (current === 0) {
+        current = 3;
+    } else {
+        current --
+    }
+    goto(current);
+}
+
+const deck = document.querySelector('.deck');
+function goto(index) {
+    if (![0,1,2,3].includes(index)) return;
+
+    cards.forEach(card => {
+        if (card.classList.contains('active')) {
+            card.classList.remove('active');
+        }
+    });
+
+    for (let i = 0; i<4; i++) {
+        cards[4 * index + i].classList.add('active');
+    }
+    deck.styleLeft = index * 1230;
+    removeMark()
+    dots[current].classList.toggle('marked');
+}
 
 
 
